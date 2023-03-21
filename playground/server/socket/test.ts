fffe150f00ba6042f0936dd62f75cf421b3ce99d
didavid61202 <didavid61202@gmail.com>
2023-03-21 19:52:40 +0800

    testing  chat

--- playground/server/socket/test.ts
+++ playground/server/socket/test.ts
@@ -3,5 +3,14 @@ import { defineIOHandler } from '../../../src/module'
 export default defineIOHandler((io) => {
   io.on('connection', (socket) => {
     console.log('Connected ', socket.id)
+
+    socket.on('message', ({ msg, to }) => {
+      console.log({from: socket.id,  msg, to })
+      if (to === '' || to === undefined)
+        io.emit('newMessage', msg)
+      else
+        io.to(to).emit('newMessage', msg)
+    })
+
   })
 })
